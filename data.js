f82f4246bf58b2d4d0b58fad7d9e9a2fb93b1e06
grifi-2809 <grifiisbad@gmail.com>
2026-04-27 00:08:03 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,152 @@
+// data.js
+
+let defaultDesc = "No description has been added to this video.";
+const myVideos = [
+    {
+        id: "vid_1", // A unique ID for the URL
+        title: "Tryon International 2*L Cross Country GoPro (5/13) Bold Impression",
+        channel: "Olivia Coolidge",
+        views: "11.1m views",
+        time: "1 hour ago",
+        videoPath: "./videos/horse.mp4", 
+        thumbnail: "", 
+        
+        description: defaultDesc
+    
+    },
+    {
+        id: "vid_2",
+        title: "Mourning Dove Coo",
+        channel: "Action Cams",
+        views: "6.9m views",
+        time: "18 Apr 2010",
+        videoPath: "./videos/dove.mp4",
+        thumbnail: "",
+        description: `Quick video of a Mourning Dove Coo. High quality video and sound.
+
+Sarcastic answers to frequently asked questions:
+1)  The name of the species is Mourning Dove.  I didn't spell it wrong.  Look it up if you don't believe me.
+2)  The title simply states what the dove is doing.  It's not mourning anything, it's just "cooing".  Furthermore, I'm not mourning the loss of a dove named "Coo".
+3)  If you're surprised because you thought an owl makes this sound, no need to comment about it.  I've already seen about 32 bazillion comments saying the same thing.
+4)  Thanks for the comments about the good quality video, but yes, I had an HD camera in 2010.  It's not THAT long ago.  Stop making me feel old.
+5)  Mourning doves are NOT extinct.  They have a status of "Least Concern" as of 2024.  Do a quick search before you make sad comments saying they're extinct.`
+
+    },
+    {
+        id: "vid_3",
+        title: "10 Minutes of Intense Bodycam Footage",
+        channel: "Trinshed",
+        views: "133 views",
+        time: "1 year ago",
+        videoPath: "./videos/bodycam.mp4",
+        thumbnail: "",
+        description: `Headphones recomended for this video
+        
+        
+        
+        
+#gameplay #games #intensegameplay #action #fps #realism #realistic #reels`
+    },
+    {
+        id: "vid_4",
+        title: "Owls of North America and their Calls",
+        channel: "3.3m views",
+        views: "4.20m views",
+        time: "7 years ago",
+        videoPath: "./videos/owl.mp4",
+        thumbnail: "",
+        description: `Owls are one of the most iconic and recognisable group of birds, with a firm grounding in culture and mythology worldwide. Discover which species can be found in North America and the unique sounds they make.
+
+
+AUDIO ATTRIBUTIONS: 
+
+Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+https://creativecommons.org/licenses/...
+
+
+
+• Barred Owl: Andy Martin- https://www.xeno-canto.org/389995  
+
+• Boreal Owl: Elias A. Ryberg- https://www.xeno-canto.org/168502  
+• Burrowing Owl: Jonas Nilsson- https://www.xeno-canto.org/275753  
+
+• Eastern Screech Owl: Todd Wilson- https://www.xeno-canto.org/153434  
+
+• Eastern Screech Owl: Paul Marvin- https://www.xeno-canto.org/278588  
+
+• Elf Owl: Scott Olmstead- https://www.xeno-canto.org/363557  
+• Ferruginous Pygmy-Owl: Dan Lane- https://www.xeno-canto.org/368470  
+• Flammulated Owl: Phoenix Birder- https://www.xeno-canto.org/178673  
+
+• Great Grey Owl: Tero Linjama- https://www.xeno-canto.org/343167  
+
+• https://www.xeno-canto.org/343168  
+• Long-eared Owl: Rob van Bemmelen- https://www.xeno-canto.org/305965  
+• Long-eared Owl: Micah Riegner- https://www.xeno-canto.org/233963  
+• https://www.xeno-canto.org/233963  
+• Mountain Pygmy Owl: Lance A. M. Benner- https://www.xeno-canto.org/202603  
+• Mountain Pygmy Owl: Paul Marvin- https://www.xeno-canto.org/147392  
+
+• Northern Hawk Owl: Karl-Birger Strann- https://www.xeno-canto.org/401784  
+
+• Northern Saw whet Owl: Micah Riegner- https://www.xeno-canto.org/236357  
+
+• Short-eared Owl: Bruce Lagerquist- https://www.xeno-canto.org/344157  
+• Short-eared Owl: https://www.xeno-canto.org/233305  
+
+• Snowy Owl: Tero Linjama-  https://www.xeno-canto.org/343144  
+• https://www.xeno-canto.org/343145  
+
+• Snowy Owl: Patrik Åberg- https://www.xeno-canto.org/277681
+• Spotted Owl – Andrew Spencer
+• Whiskered Screech Owl: Richard E. Webster- https://www.xeno-canto.org/322589
+• Barn Owl: Paul Marvin- https://www.xeno-canto.org/413554 
+
+
+Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+https://creativecommons.org/licenses/...
+
+
+• Great Horned Owl: Andy Martin- https://www.xeno-canto.org/386841  
+• Barred Owl: Andrew Spencer- https://www.xeno-canto.org/48644  
+• Great Horned Owl: Tayler Brooks- https://www.xeno-canto.org/38000   
+• Great Horned Owl: Andy Martin- https://www.xeno-canto.org/386841  
+• Northern Hawk Owl: Terje Kolaas- https://www.xeno-canto.org/263932  
+• Northern Pygmy Owl: Andrew Spencer- https://www.xeno-canto.org/104673  
+• Western Screech Owl: Tayler Brooks- https://www.xeno-canto.org/61474
+
+IMAGE ATTRIBUTIONS:
+
+Attribution 2.0 Generic (CC BY 2.0)
+
+• Boreal Owl: Tim Rains - https://www.flickr.com/photos/denalinps/
+• Whiskered Screech Owl: Bettina Arrigoni - https://commons.wikimedia.org/wiki/Fi...
+
+Attribution-ShareAlike 2.0 Generic (CC BY-SA 2.0)
+
+• Elf Owl: Dominic Sherony - https://www.flickr.com/photos/9765210...
+• Ferruginous Pygmy Owl: Dominic Sherony - https://commons.wikimedia.org/wiki/Fi...
+• Mountain Pygmy Owl: Dominic Sherony - https://en.wikipedia.org/wiki/Mountai...
+
+Attribution-NonCommercial-NoDerivs 2.0 Generic (CC BY-NC-ND 2.0)
+
+• Flammulated Owl: Julio Mulero - https://www.flickr.com/photos/juliom/...
+• Northern Pygmy Owl: Frank D. Lospalluto - https://www.flickr.com/photos/chloesv...
+• Spotted Owl: Frank D. Lospalluto - https://www.flickr.com/photos/chloesv...
+Attribution 3.0 Unported (CC BY 3.0)
+• Northern Hawk Owl: BramtK - https://observation.org/photos/4330246/
+• Snowy Owl: Jongsun Lee - https://commons.wikimedia.org/wiki/Fi...
+
+Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+
+• Barred Owl: Mdf - https://en.wikipedia.org/wiki/Barred_...
+• Burrowing Owl: http://www.birdphotos.com - https://commons.wikimedia.org/wiki/Fi...
+• Great Grey Owl: Olaf Oliviero Riemer - https://commons.wikimedia.org/wiki/Fi...
+
+Attribution 4.0 International (CC BY 4.0)
+
+• Northern Saw-whet Owl: Andy Reago & Chrissy McClarren - https://commons.wikimedia.org/wiki/Fi...
+• Short-eared Owl: Gregory "Slobirdr" Smith - https://commons.wikimedia.org/wiki/Fi...
+• Western Screech-Owl: Dominic Sherony - https://www.flickr.com/photos/9765210...`
+    }
+];
